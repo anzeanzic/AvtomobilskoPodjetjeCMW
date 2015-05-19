8a6a917c2d197535701c514f81619f28aa658a8b
lostje <anze.anzic@siol.net>
2015-05-19 23:42:46 +0200

    Final version: - added CSS - added images - added select filter by car name - added writing of the email

--- APCMW.js
+++ APCMW.js
@@ -1,26 +1,34 @@
 angular.module('APCMW', []);
 angular.module('APCMW').controller('HttpController', function($scope, $http) {
-	$scope.carsArray = [];
-	$scope.timeslotsArray = []; // javascript map recimo -> gledaš kater termin je vezan na avto (ng-repeat v ng-repeat)
+	$scope.error = false;
+	$scope.cars = []; // hashmap
 	GetCars();
 
     $scope.reserveTimeslot = function(timeslotID) {
-    	$http.post('http://smartninja.betoo.si/api/CMW/reservations', { timeslotId: timeslotID, email: "test@gmail.com" }).success(function(data, status, headers, config) {
-
-    	}).error(function(data, status, headers, config) {
-
-    	});
+    	var email = prompt("Please enter your email:");
+    	if (email != null) {
+	    	$http.post('http://smartninja.betoo.si/api/CMW/reservations', { timeslotId: timeslotID, email: email }).success(function(data, status, headers, config) {
+	    		alert("Successfully reserved!");
+	    	}).error(function(data, status, headers, config) {
+	    		alert("Reservation failed. Please try again!");
+	    	});
+	    }
     }
 
     function GetTimeslots() {
 	    $http.get('http://smartninja.betoo.si/api/CMW/timeslots')
 	    .success(function(data, status, headers, config)
 	    {
-	    	$scope.timeslotsArray = data;
+	    	for (var i = 0; i < data.length; i++) {
+	    		if (data[i].carId in $scope.cars) {
+	    			$scope.cars[parseInt(data[i].carId)].timeslots.push(data[i]);
+	    		}
+	    	}
 	    })
 	    .error(function(data, status, headers, config)
 	    {
-	    	alert("Ne dela"); // TODO
+	    	$scope.error = true;
+	    	$scope.errorDesc = 'Error getting data from server. Please try refreshing the site!';
 	    });
     }
 
@@ -28,25 +36,18 @@ angular.module('APCMW').controller('HttpController', function($scope, $http) {
 	    $http.get('http://smartninja.betoo.si/api/CMW/cars')
 	    .success(function(data, status, headers, config)
 	    {
-	    	$scope.carsArray = data;
+	    	for (var i = 0; i < data.length; i++) {
+	    		$scope.cars[parseInt(data[i].id)] = data[i];
+	    		$scope.cars[parseInt(data[i].id)].timeslots = [];
+	    	}
 	    	GetTimeslots();
 	    })
 	    .error(function(data, status, headers, config)
 	    {
-	    	alert("Ne dela"); // TODO
+	    	$scope.error = true;
+	    	$scope.errorDesc = 'Error getting data from server. Please try refreshing the site!';
 	    });
     }
 
     // TODO: naredil filter, select, karkoli
-    // TODO: poglej, ce je skupina undefined
 });
-
-// Uporabljaj POSTMAN - Chrone extension
-
-/*
-DN
-Sproti pokaži rezultat loterije
-
-
-chain promise
-*/
